4c3949303209ebea062f6e244d1e57a1786115e8
Hypothet <76666251+Hypothet@users.noreply.github.com>
2022-05-07 14:13:56 +0100

    Create NameParts.js

---
+++ NameParts.js
@@ -0,0 +1,80 @@
+export default class NameParts{
+
+    nameParts = [
+        "ser",
+        "dov",
+        "ver",
+        "ko",
+        "mel",
+        "den",
+        "sien",
+        "ser",
+        "tol",
+        "kent",
+        "lorn",
+        "tis",
+        "bel",
+        "font",
+        "luv",
+        "nile",
+        "emosh",
+        "rec",
+        "shull",
+        "pveur",
+        "dorfe",
+        "fvek",
+        "erfa",
+        "eiya",
+        "hun",
+        "sik",
+        "do",
+        "ka",
+        "col",
+        "zva",
+        "kant",
+        "tie",
+        "el",
+        "sk",
+        "ji",
+        "qu",
+        "fer",
+        "fel",
+        "pli",
+        "dle",
+        "ho",
+        "ket",
+        "zva",
+        "luh",
+        "land",
+        "qit",
+        "sef",
+        "op",
+        "ael",
+        "wer",
+        "rye",
+        "kve",
+        "pul",
+        "ove",
+        "orv"
+
+    
+    
+    
+    
+    ]
+
+    suffix = [
+        "Empire",
+        "Republic",
+        "Commonwealth",
+        "Order",
+        "Federation",
+        "Trade League",
+        "","","","","","","","","","","","","","","",""
+    ]
+
+    prefix = [
+        "cult of"
+
+    ]
+}
