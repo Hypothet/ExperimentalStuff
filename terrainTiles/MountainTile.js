33d48699ba71ddc09d1eec5d32f2d21ee299b3ef
Hypothet <76666251+Hypothet@users.noreply.github.com>
2022-05-09 20:51:12 +0100

    Create MountainTile.js

---
+++ terrainTiles/MountainTile.js
@@ -0,0 +1,8 @@
+export default class MountainTile{
+    name = "mountain";
+    geoDefense = 10.5 // 150% defense
+    geoAttack = .6 // 70% attack
+    geoPopGrowth = .5 //50% pop growth
+    color = "#919394";
+    isMoveable = true
+}
